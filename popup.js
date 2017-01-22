1974a0f487ba2c5060c3066011289461ce97bf78
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-01-22 11:09:03 +0530

    made js more modular

--- popup.js
+++ popup.js
@@ -16,29 +16,24 @@ function populateImage(platform, index){
     elem.src= "./img/he32.png"; 
   }
 }
+function displayData(obj){
+  var i = 0;
+  for(i=0 ; i<5; i++)
+  {
+    var j = i+1;
+    //contest code
+    var elem = document.getElementById('span'+j);
+    elem.innerHTML = obj.result.upcoming_contests[i].code; 
+    //contest start time
+    var elem2 = document.getElementById('minispan'+j);
+    elem2.innerHTML = obj.result.upcoming_contests[i].start_time;
+    //contest image
+    populateImage(obj.result.upcoming_contests[i].platform,i);
+  }
 
+}
 document.addEventListener('DOMContentLoaded', function() {
- /*
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-
-    });
-  }, false);*/
-
+ 
   var xhr = new XMLHttpRequest();;
   xhr.onreadystatechange = function()
   {
@@ -49,26 +44,29 @@ document.addEventListener('DOMContentLoaded', function() {
         var obj = JSON.parse(xhr.responseText);
         //we got the json object
         //now fill all spans with json data through iteration
-        var i = 0;
-        for(i=0 ; i<5; i++)
-        {
-          var j = i+1;
-          //contest code
-          var elem = document.getElementById('span'+j);
-          elem.innerHTML = obj.result.upcoming_contests[i].code; 
-          //contest start time
-          var elem2 = document.getElementById('minispan'+j);
-          elem2.innerHTML = obj.result.upcoming_contests[i].start_time;
-          //contest image
-          populateImage(obj.result.upcoming_contests[i].platform,i);
-        }
+        displayData(obj);
         
       }
-      else{
+      else
+      {
         alert("Request was unsuccessfull");
       }
     }
   };
   xhr.open("get","https://tranquil-caverns-50595.herokuapp.com/",true);
   xhr.send(null);
+
+
+
+  var checkButton = document.getElementById('showmorebtn');
+  checkButton.addEventListener('click', function() 
+  {
+
+
+
+  }, false);
+
+
+
+
 }, false);
